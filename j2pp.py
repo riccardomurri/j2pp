2e3194e19520f339863c071325a352f3a8b36335
Riccardo Murri <riccardo.murri@gmail.com>
2015-03-06 14:25:41 +0100

    Add copyright and licensing info.

--- j2pp.py
+++ j2pp.py
@@ -3,6 +3,23 @@
 """
 A preprocessor using Jinja2 syntax.
 """
+##
+# Copyright (C) 2015 S3IT, Zentrale Informatik, University of Zurich.
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/#GPL>.
+##
 __docformat__ = 'reStructuredText'
 
 
@@ -272,7 +289,7 @@ def parse_defines(defs, default=1,
                     if lengthen:
                         if logger:
                             logger.warning(
-                                "Assignment to key '%s' overwrites existing key/value '%s=%r'",
+                                "Assignment to key '%s' overwrites existing key/value '%s=%s'",
                                 k, str.join('.', ks[:n+1]), target[h])
                         target[h] = {}
                     else:
